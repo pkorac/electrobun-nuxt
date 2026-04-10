6564a7b06174b10d4ba008164e3777234b08fb59
Peter Koraca <peter.koraca@gmail.com>
2026-04-10 21:34:28 +0200

    added two pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class="w-full h-full flex items-center justify-center">
-		Hello world of bun
+	<div class="w-full h-full flex flex-col items-center justify-center gap-4">
+		<h1 class="text-4xl font-semibold">ElectroNuxt</h1>
+
+		<p>Hello world of bun</p>
+		<UButton to="/the-other-page" label="The other page" variant="soft" />
 	</div>
 </template>
 <script setup lang="ts"></script>
---
+++ app/pages/the-other-page.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="w-full h-full flex flex-col items-center justify-center gap-4">
+		<h1 class="text-4xl font-semibold">The other page</h1>
+
+		<UButton to="/" label="Back" variant="soft" />
+	</div>
+</template>
+<script setup lang="ts"></script>
